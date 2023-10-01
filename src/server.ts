8a24c62918315144be9a6922c00954e6a39012bb
rifatjahan823 <rifatjahan823@gmail.com>
2023-10-01 10:24:22 +0600

    handle unhandledRejection,sigterm in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,50 @@
 /* eslint-disable no-console */
 // getting-started.js
 import mongoose from 'mongoose'
-import app from './app';
-import config from './config';
+import app from './app'
+import config from './config'
+import {Server} from 'http'
+
+
+let server:Server
 
 async function main() {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/test');
-    app.listen(config.port,async()=>{
-        console.log(`Listening Port ${config.port}`)
+    await mongoose.connect(`${config.database_url}`)
+   server= app.listen(config.port, async () => {
+      console.log(`Listening Port ${config.port}`)
     })
   } catch (error) {
     console.log(error)
   }
+process.on('unhandledRejection',error=>{
+ if(server){
+  server.close(()=>{
+    console.log(error);
+    process.exit(1)
+  })
+ }else{
+  process.exit(1)
+ }
+})
+
 }
 
 main().catch(err => console.log(err))
+
+
+process.on('uncaughtException',error=>{
+  console.log(error);
+  process.exit(1)
+})
+
+process.on('SIGTERM',()=>{
+  console.log('sigterm recieved')
+  if(server){
+    server.close(()=>{
+      process.exit(1)
+    })
+  }else{
+    process.exit(1)
+  }
+})
