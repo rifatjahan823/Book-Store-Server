8558323e496d96f7d03d4efb2444209c4813f7fd
rifatjahan823 <rifatjahan823@gmail.com>
2023-10-01 18:01:26 +0600

    create validateRequest function

--- src/shared/sendResponse.ts
+++ src/shared/sendResponse.ts
@@ -1,16 +1,16 @@
-import { Response } from 'express';
+import { Response } from 'express'
 
 type responseData<T> = {
-  statusCode: number;
-  success: boolean;
-  message?: string | null;
+  statusCode: number
+  success: boolean
+  message?: string | null
   meta?: {
-    page: number;
-    limit: number;
-    total: number;
-  };
-  data?: T | null;
-};
+    page: number
+    limit: number
+    total: number
+  }
+  data?: T | null
+}
 export const sendResponse = <T>(res: Response, data: responseData<T>): void => {
   const reponseData: responseData<T> = {
     statusCode: data.statusCode,
@@ -18,7 +18,7 @@ export const sendResponse = <T>(res: Response, data: responseData<T>): void => {
     message: data.message || null,
     meta: data.meta || null || undefined,
     data: data.data || null,
-  };
+  }
 
-  res.status(data.statusCode).json(reponseData);
-};
+  res.status(data.statusCode).json(reponseData)
+}
