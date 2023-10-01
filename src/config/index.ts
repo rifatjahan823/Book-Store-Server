b6d7370f6f0afa3f6f3f204769c3b23567629993
rifatjahan823 <rifatjahan823@gmail.com>
2023-10-01 10:56:54 +0600

    create ApiError

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,5 +5,5 @@ dotenv.config({ path: path.join(process.cwd(), '.env') })
 export default {
   node_env: process.env.NODE_ENV,
   port: process.env.PORT,
-  database_url:process.env.DATABASE_URL,
+  database_url: process.env.DATABASE_URL,
 }
--- src/server.ts
+++ src/server.ts
@@ -3,48 +3,45 @@
 import mongoose from 'mongoose'
 import app from './app'
 import config from './config'
-import {Server} from 'http'
+import { Server } from 'http'
 
-
-let server:Server
+let server: Server
 
 async function main() {
   try {
     await mongoose.connect(`${config.database_url}`)
-   server= app.listen(config.port, async () => {
+    server = app.listen(config.port, async () => {
       console.log(`Listening Port ${config.port}`)
     })
   } catch (error) {
     console.log(error)
   }
-process.on('unhandledRejection',error=>{
- if(server){
-  server.close(()=>{
-    console.log(error);
-    process.exit(1)
+  process.on('unhandledRejection', error => {
+    if (server) {
+      server.close(() => {
+        console.log(error)
+        process.exit(1)
+      })
+    } else {
+      process.exit(1)
+    }
   })
- }else{
-  process.exit(1)
- }
-})
-
 }
 
 main().catch(err => console.log(err))
 
-
-process.on('uncaughtException',error=>{
-  console.log(error);
+process.on('uncaughtException', error => {
+  console.log(error)
   process.exit(1)
 })
 
-process.on('SIGTERM',()=>{
+process.on('SIGTERM', () => {
   console.log('sigterm recieved')
-  if(server){
-    server.close(()=>{
+  if (server) {
+    server.close(() => {
       process.exit(1)
     })
-  }else{
+  } else {
     process.exit(1)
   }
 })
