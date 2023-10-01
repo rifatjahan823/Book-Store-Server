a0815ff323a15c98ab0647ded9ea3e33889a4646
rifatjahan823 <rifatjahan823@gmail.com>
2023-10-01 17:55:37 +0600

    add sendresponse function

--- src/shared/catchAsync.ts
+++ src/shared/catchAsync.ts
@@ -1,8 +1,10 @@
-import { NextFunction, Request, RequestHandler, Response } from 'express';
-export const catchAsync=(fn: RequestHandler)=>async(req: Request, res: Response, next: NextFunction)=>{
+import { NextFunction, Request, RequestHandler, Response } from 'express'
+export const catchAsync =
+  (fn: RequestHandler) =>
+  async (req: Request, res: Response, next: NextFunction) => {
     try {
-        await fn(req, res, next);
-      } catch (error) {
-        next(error);
-      }
-}
+      await fn(req, res, next)
+    } catch (error) {
+      next(error)
+    }
+  }
