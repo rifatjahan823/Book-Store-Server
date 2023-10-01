805153440e88d89fa0f98e32ce741130828a82d0
rifatjahan823 <rifatjahan823@gmail.com>
2023-10-01 18:49:55 +0600

    create userinterface

--- src/middleware/validateRequest.ts
+++ src/middleware/validateRequest.ts
@@ -1,5 +1,5 @@
-import { NextFunction, Request, Response } from 'express';
-import { AnyZodObject, ZodEffects } from 'zod';
+import { NextFunction, Request, Response } from 'express'
+import { AnyZodObject, ZodEffects } from 'zod'
 
 const validateRequests =
   (schema: AnyZodObject | ZodEffects<AnyZodObject>) =>
@@ -10,11 +10,11 @@ const validateRequests =
         query: req.query,
         params: req.params,
         cookies: req.cookies,
-      });
-      return next();
+      })
+      return next()
     } catch (error) {
-      next(error);
+      next(error)
     }
-  };
+  }
 
-export default validateRequests;
+export default validateRequests
---
+++ src/modules/user/user.validation.ts
@@ -0,0 +1,16 @@
+import { Model } from "mongoose"
+
+// 1. Create an interface representing a document in MongoDB.
+type UserName = {
+    firstName: string
+    lastName: string
+  }
+  export type IUser = {
+    name: UserName
+    email:string;
+    password: string;
+    needsPasswordChange:true|false;
+  }
+  
+  // Create a new Model type that knows about IUserMethods...
+  export type UserModel = Model<IUser, Record<string, unknown>>
